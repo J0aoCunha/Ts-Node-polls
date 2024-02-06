6c6e4de87169b93949641219db3d3b9a2fd183ea
J0aoCunha <jvjunger20123@gmail.com>
2024-02-06 20:44:58 -0300

    feat: criando o metodo get de uma poll e suas options

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,30 @@
+import z from "zod"
+import { prisma } from "../../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export const getPoll = async (app: FastifyInstance) => {
+  app.get("/polls/:pollId", async  (req, reply)=>{
+
+  const getPollBody = z.object({
+    pollId: z.string().cuid()
+  })
+
+  const { pollId } = getPollBody.parse(req.params)
+
+  const poll = await prisma.poll.findUniqueOrThrow({
+    where:{
+      id: pollId,
+    },
+    include:{
+      options:{
+        select:{
+          poll_Id: true,
+          title: true
+        }
+      }
+    }
+  })
+
+  
+  return reply.send({ poll })
+})}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,4 +1,13 @@
-import { app } from "./app"
+import fastify from "fastify"
+import { createPoll } from "./routes/create-poll"
+import { getPoll } from "./routes/get-poll"
+import { voteOnPoll } from "./routes/vote-on-poll"
+
+const app = fastify()
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({
   host: "0.0.0.0",
